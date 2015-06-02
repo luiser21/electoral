3b2cdbfbeaf1d7d07638ac17abcdbb70bc4c0128
LUIS CARDENAS <luishernando18@hotmail.com>
2015-06-01 21:58:30 -0500

    cambios cargues cedulas recogidas

--- example_doc_2.php
+++ example_doc_2.php
@@ -8,7 +8,6 @@ include_once "includes/GestionBD.new.class.php";
 $data->read('Excel/cargas/cedulas_recojidas.xls');
 $y=0;
 $registros=count($data->sheets[0]['cells']);
-
 for ($i = 2; $i <= $registros; $i++) {
 	$cedula_simpatizante[$y]=trim($data->sheets[0]['cells'][$i][1]);
 	@$alcaldia[$y]=trim($data->sheets[0]['cells'][$i][2]);
@@ -16,23 +15,26 @@ for ($i = 2; $i <= $registros; $i++) {
 	$y++;
 	
 }
-
 $DBGestion = new GestionBD('AGENDAMIENTO');	
-
+$tipo=0;
 for($i=0; $i<$registros-1; $i++){		
 	$sql="select IDPUESTO from cedulas_inscritas where CEDULAS=".trim($cedula_simpatizante[$i]);
 	$DBGestion->ConsultaArray(strtoupper($sql));	
 	$idpuesto=$DBGestion->datos;
-	//strtoupper($alcaldia[$i]);
-	if(!empty($idpuesto[0])){
-		$sql="INSERT INTO MIEMBROS (NOMBRES, CEDULA, MUNICIPIO, IDPUESTOSVOTACION, IDLIDER, OCUPACION) VALUES 
-		('NO APLICA',".trim($cedula_simpatizante[$i]).",845,".$idpuesto[0]['IDPUESTO'].",44,'VOLUNTARIADO')";	
-		
-		echo $sql;
-		//$DBGestion->Consulta($sql);
+	if(strtoupper(@$alcaldia[$i])=='X'){
+		$tipo=3;
 	}else{
-		//echo 'pailas';
+		$tipo=4;
 	}
-}
-
-	?>
+	if(!empty($idpuesto[0])){
+		$sql="INSERT INTO recoleccion_cedulas (IDPUESTO, CEDULAS,CANDIDATO, TIPOELECCION) VALUES 
+		(".$idpuesto[0]['IDPUESTO'].",".trim($cedula_simpatizante[$i]).",23,".$tipo.")";		
+		echo $sql;
+		$DBGestion->Consulta($sql);
+	}else{
+		$sql="INSERT INTO recoleccion_cedulas (CEDULAS,CANDIDATO, TIPOELECCION) VALUES 
+		(".trim($cedula_simpatizante[$i]).",23,".$tipo.")";			
+		echo '<br/>'.$sql;
+		$DBGestion->Consulta($sql);
+	}
+}	?>
